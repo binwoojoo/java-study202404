96b455599a6932955c07c5e85cee2530886b3a3a
binwoojoo <oooo940212@gmail.com>
2024-04-18 00:09:29 +0900

    # 상속 상속 복습

--- src/day07/player/Warrior.java
+++ src/day07/player/Warrior.java
@@ -15,7 +15,7 @@ public class Warrior extends Player {
 
     public void Dash(Warrior target) {
         System.out.println("Dash 스킬을 사용합니다.");
-        System.out.println(target.nickname+"은 개쳐맞았습니다.");
+        System.out.println(target.nickname + "은 개쳐맞았습니다.");
     }
 
     // 오버라이딩: 부모가 물려준 메서드를 고쳐 쓰는 것
@@ -28,13 +28,4 @@ public class Warrior extends Player {
         System.out.println("# rage: " + this.rage);
     }
 
-    // 오버라이딩: 부모가 물려준 메서드를 고쳐 쓰는 것
-    // 규칙: 부모가 물려준 형태를 유지할 것(파라미터, 리턴타입, 이름)
-    //  접근 제한자는 부모보다 more public 할 것!
-
-    @Override // 오버라이딩 룰을 지켰는지 검증
-    public void showStatus() {
-        super.showStatus();
-        System.out.println("# rage: " + this.rage);
-    }
 }
