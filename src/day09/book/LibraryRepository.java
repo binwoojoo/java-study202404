1c29e1b5d304793d3c93f252c0fef9683109499c
oooowb <oooo940212@gmail.com>
2024-04-19 15:59:13 +0900

    #객체 지향  종합 퀴즈!!!!

---
+++ src/day09/book/LibraryRepository.java
@@ -0,0 +1,70 @@
+package day09.book;
+
+// 역할: 도서관리 앱에서 사용하는 데이터들을 관리하는 객체
+// 관리할 데이터: 우리 회원이 누구?  우리는 어떤책들을 갖고 있는가?
+public class LibraryRepository {
+
+    private static BookUser user; // 회원은 한명
+    private static BookList bookList;  // 책은 여러개
+
+    // 정적 초기화자 : static필드 초기값 설정
+    static {
+        bookList = new BookList();
+
+        bookList.push(new CookBook("기적의 집밥책", "김해진", "청림라이프", true));
+        bookList.push(new CartoonBook("떨어지면 끝장맨", "스에노부 케이코", "대원씨아이", 18));
+        bookList.push(new CartoonBook("원펀맨", "One", "대원씨아이", 15));
+        bookList.push(new CookBook("삐뽀삐뽀 119 이유식", "하정훈", "유니책방", false));
+        bookList.push(new CookBook("오늘은 아무래도 덮밥", "이마이 료", "참돌", true));
+        bookList.push(new CartoonBook("이세계로 전이했으니 치트를 살려 마법검사가 되기로 했다", "Shinkoshoto", "대원씨아이", 12));
+    }
+
+    // 저장소에 user를 저장
+    public void saveBookUser(BookUser bookUser) {
+        user = bookUser;
+    }
+
+    // 저장된 user 정보를 리턴
+    public BookUser getUserInfo() {
+        return user;
+    }
+
+    // 책 정보가 들어 있는 배열을 리턴
+    public Book[] getAllBooksInfo() {
+        return bookList.getbArr();
+    }
+
+    /**
+     * 주어진 책 번호에 맞는 책이 대여 가능한지에 대한 상태를 리턴
+     *
+     * @param bookNum - 주어진 책 번호
+     * @return 대여가능한 상태를 반환
+     */
+    public RentStatus rentBook(String bookNum) {
+        // 1. 책 번호에 해당하는 책 정보 가져오기
+        Book wishBook = bookList.get(Integer.parseInt(bookNum) - 1);
+        System.out.println(wishBook.info());
+        // 2. 이 책을 대여할 수 있는지 검증
+        // 2-1. 요리책일 경우
+        if (wishBook instanceof CookBook) {
+            // 3. 쿠폰 유무를 확인
+            if (((CookBook) wishBook).isCoupon()) {
+                // 회원의 쿠폰갯수를 업데이트
+                user.setCouponCount(user.getCouponCount() + 1);
+                return RentStatus.RENT_SUCCESS_WITH_COUPON;
+            } else {
+                return RentStatus.RENT_SUCCESS;
+            }
+        }
+        // 2-2. 만화책일 경우
+        else if (wishBook instanceof CartoonBook) {
+            // 3. 회원의 연령이 만화책의 제한연령보다 높은지 확인
+            if (user.getAge() >= ((CartoonBook) wishBook).getAccessAge()) {
+                return RentStatus.RENT_SUCCESS;
+            } else {
+                return RentStatus.RENT_FAIL;
+            }
+        }
+        return RentStatus.RENT_FAIL;
+    }
+}
---
+++ src/day09/book/LibraryView.java
@@ -0,0 +1,132 @@
+package day09.book;
+
+import static util.SimpleInput.*;
+
+// 역할: 도서관리 프로그램에서 입출력을 담당하는 객체
+public class LibraryView {
+
+    private LibraryRepository repository;
+
+    public LibraryView() {
+        this.repository = new LibraryRepository();
+    }
+
+    // 회원 정보를 입력받는 기능
+    public void makeNewBookUser() {
+        System.out.println("\n# 회원 정보를 입력해주세요.");
+        String name = input("# 이름: ");
+        int age = Integer.parseInt(input("# 나이: "));
+        Gender gender = inputGender();
+
+        // 입력된 데이터를 저장시켜야 함.
+        repository.saveBookUser(new BookUser(name, age, gender, 0));
+    }
+
+    // 성별을 정확히 입력할때가지 무한히 입력받고
+    // 정확히 입력하면 해당 성별 문자를 리턴
+    private Gender inputGender() {
+        while (true) {
+            String gender = input("# 성별(M/F): ").toUpperCase();
+            if (gender.startsWith("M")) return Gender.MALE;
+            if (gender.startsWith("F")) return Gender.FEMALE;
+            System.out.println("\n# 성별을 잘못 입력했습니다.");
+        }
+    }
+
+    // 메뉴를 출력하는 기능
+    public void showMainScreen() {
+        System.out.println("\n============ 도서 메뉴 ==============");
+        System.out.println("# 1. 마이페이지");
+        System.out.println("# 2. 도서 전체 조회");
+        System.out.println("# 3. 도서 제목으로 검색");
+        System.out.println("# 4. 도서 대여하기");
+        System.out.println("# 9. 프로그램 종료하기");
+    }
+
+    // 코드의 흐름을 캡슐화
+    public void start() {
+        makeNewBookUser();
+
+        while (true) {
+            showMainScreen();
+            String menuNum = input("- 메뉴 번호 : ");
+
+            switch (menuNum) {
+                case "1":
+                    displayUser();
+                    break;
+                case "2":
+                    displayAllBooks();
+                    break;
+                case "3":
+                    searchBookName();
+                    break;
+                case "4":
+                    rentBook();
+                    break;
+                case "9":
+                    System.out.println("# 프로그램을 종료합니다!!");
+                    return;
+                default:
+                    System.out.println("# 올바른 메뉴 번호를 입력하세요!");
+            }
+        }
+    }
+
+    // 회원 정보를 출력
+    private void displayUser() {
+        System.out.println("\n=============== 회원 정보 ================");
+        BookUser user = repository.getUserInfo();
+        System.out.println("# 이름: " + user.getName());
+        System.out.println("# 나이: " + user.getAge());
+        System.out.println("# 성별: " + user.getGenderToString());
+        System.out.println("# 쿠폰갯수: " + user.getCouponCount());
+    }
+
+    // 전체 도서 정보를 출력
+    private void displayAllBooks() {
+        System.out.println("\n=============== 전체 도서 목록 ================");
+        Book[] informationList = repository.getAllBooksInfo();
+
+        for (int i = 0; i < informationList.length; i++) {
+            Book book = informationList[i];
+            System.out.printf("%d. %s\n", i + 1, book.info());
+        }
+
+    }
+
+    // 책을 입력한 검색어로 찾기
+    private void searchBookName() {
+        String searchName = input("# 검색어: ");
+        Book[] informationList = repository.getAllBooksInfo();
+        System.out.printf("============ [%s] 검색 결과 ============\n", searchName);
+        int i = -1;
+        for (Book book : informationList) {
+            if (book.getTitle().contains(searchName)) {
+                System.out.println(book.info());
+                i = 1;
+            }
+        }
+        if (i == -1) {
+            System.out.printf("[%s]을(를) 포함하는 검색 결과가 없습니다.\n", searchName);
+        }
+    }
+
+    // 대여 가능한 도서 정보 찾기
+    public void rentBook() {
+        displayAllBooks();
+        String bookNum = input("- 대여할 도서 번호 입력: ");
+        // 저장소에다가 대여가능한지 여부 검증
+        RentStatus status = repository.rentBook(bookNum);
+
+        if (status == RentStatus.RENT_SUCCESS_WITH_COUPON) {
+            System.out.println("# 쿠폰과 함께 대여 성공");
+        } else if (status == RentStatus.RENT_SUCCESS) {
+            System.out.println("# 대여 성공");
+        } else {
+            System.out.println("# 대여 실패");
+        }
+    }
+}
+
+
